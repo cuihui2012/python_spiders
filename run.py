c7b3d265909eaf1a4d72343c047d3db792baba3c
cuihui2012 <751670441@qq.com>
2020-07-30 18:20:27 +0800

    修改生产部署配置

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ from spider_server.conf.config_util import ConfigUtil
 if __name__ == '__main__':
     # 启动服务
     app.run(host=ConfigUtil().get("SERVER", "SERVER_HOST"), port=int(ConfigUtil().get("SERVER", "SERVER_PORT")),
-            debug=bool(ConfigUtil().get("SERVER", "SERVER_DEBUG")))
+            debug=bool(int(ConfigUtil().get("SERVER", "SERVER_DEBUG"))))
