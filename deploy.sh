bdefa5b54e911ec349ffdc4ddbd0d2631ac421f4
cuihui2012 <751670441@qq.com>
2020-12-23 14:54:10 +0800

    修改自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-##编译+部署项目
-
-##需要export PROJ_PATH=jenkins任务在部署机器上的路径,已在任务中的shell脚本上配置
-##编译
-cd $PROJ_PATH/python_spiders
-##war包部署
+##部署项目
 cp -rf $PROJ_PATH/python_spiders/* /root/python_spiders/
-##重启docker容器
-#docker restart 80a011bf441a
+##重启服务
+/root/restart-server.sh -s
